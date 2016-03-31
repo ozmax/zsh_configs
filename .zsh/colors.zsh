5ffd95a6ea7803ca13015f5c6f5102d8c246cf48
Georgios Tsakalos <ozmax.planet@gmail.com>
2016-03-31 13:02:28 +0300

    add ls colors for dir fi ln ex

--- .zsh/colors.zsh
+++ .zsh/colors.zsh
@@ -1,5 +1,20 @@
-autoload -U colors && colors
+autoload -Uz colors && colors
 
-unset LSCOLORS
-export CLICOLOR=1
-export LS_COLORS=exfxcxdxbxegedabagacad
+export CLICOLOR=0
+
+### LS COLORS ###
+
+# directory color
+LS_COLORS='di=1;34:'
+
+#file color
+LS_COLORS=$LS_COLORS:'fi=4;93:'
+
+# symlink color
+LS_COLORS=$LS_COLORS:'ln=0;36:'
+
+# executable color
+LS_COLORS=$LS_COLORS:'ex=0;31:'
+export LS_COLORS
+
+#################
