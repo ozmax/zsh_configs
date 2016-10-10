c36721b3017842648478ed684cbda6cecc67cdbf
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2016-10-10 12:02:09 +0300

    add function tools and bind all keys

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -1,5 +1,1 @@
-bindkey "^R"      history-incremental-search-backward
-bindkey "[B"      history-search-forward              # down arrow
-bindkey "[A"      history-search-backward             # up arrow
-bindkey "^A"      beginning-of-line
-bindkey "^E"      end-of-line
+bindkey -e
--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,3 +1,5 @@
 export LSCOLORS=Exfxcxdxbxegedabagacad
 export EDITOR='vim'
 export DISABLE_AUTO_TITLE='true'
+
+export PATH=/opt/sublime_text_3:$PATH
